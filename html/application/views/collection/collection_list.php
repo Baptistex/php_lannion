62f7591f7a6c7f51278c90f61338bfd252e556db
tlaharotte <thomas.laharotte@etudiant.univ-rennes1.fr>
2020-11-18 15:21:40 +0100

    Encore une maj style

--- html/application/views/collection/collection_list.php
+++ html/application/views/collection/collection_list.php
@@ -21,5 +21,23 @@
     </div>
     <?php endforeach ?>
 </div>
+<div id="carte" class="col-12 offset-1 d-lg-none">
+<?php foreach($collectionlist as $jeu):?>
+    <div class="example-1 card ">
+        <div class="wrapper">
+            <img src="<?php echo $jeu['couverture']?>" width="272.95">
+            <div class="date">
+                <span class="day"><?php echo $jeu['sortie']?></span>
+            </div>
+            <div class="data">
+                <div class="content">
+                    <h1 class="title"> <a href="<?php echo base_url()."jeux/game/".$jeu["id"]?>" class="inner"><?php echo $jeu['titre']?></a></h1>
+                    <p class="text"><?php echo $jeu['description']?></p>
+                </div>
+            </div>
+        </div>
+        <?php echo anchor('collection/delete/'.$jeu["id"],'<button>Supprimer</button>');?>
+    </div>
+    <?php endforeach ?>
+</div>
 <?php echo $delete?>
-
