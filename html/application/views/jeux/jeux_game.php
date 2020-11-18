485276e0f0535758b077a997a3726b85871edea1
tlaharotte <thomas.laharotte@etudiant.univ-rennes1.fr>
2020-11-18 17:38:37 +0100

    mise a jour un peu de tout

--- html/application/views/jeux/jeux_game.php
+++ html/application/views/jeux/jeux_game.php
@@ -1,16 +1,18 @@
 <h2><?php echo $title ?></h2>
 
-<div class="row col-lg-12 offset-lg-3">
+<div id="jeux_game">
+<div id="game" class="row col-lg-12">
     <div>
         <img src="<?php echo $gamedata['couverture']?>" width="300" height="600">
     </div>
     <div>
-        <label>Nom du jeu : <?php echo $gamedata['titre']?></label><br>
-        <label>ID : <?php echo $gamedata['id']?></label><br>
-        <label>Date de sortie : <?php echo $gamedata['sortie']?></label>
+        <label id="labeljeux">Nom du jeu : <?php echo $gamedata['titre']?></label><br>
+        <label id="labeljeux">ID : <?php echo $gamedata['id']?></label><br>
+        <label id="labeljeux">Date de sortie : <?php echo $gamedata['sortie']?></label>
     </div>
 </div>
-<label id="description">Description : <?php echo $gamedata['description']?></label>
+<label id="description" class="col-lg-8">Description : <?php echo $gamedata['description']?></label>
+</div>
 
 <div id="bouton">
     <a href="index.html"><button>Retour</button></a>
